8bf791598e5e07725eec21f5514176d2cf8ecd9a
soylentkim <gloomy.penguin.kim@gmail.com>
2024-11-14 10:35:54 -0600

    tried dowloading photos...

---
+++ wgetfile.sh
@@ -0,0 +1,126 @@
+# 1
+wget -O /home/kim/Documents/travel-data/images/fiordland-national-park-new-zealand.jpg "https://www.newzealand.com/assets/Tourism-NZ/Fiordland/56113b64e7/img-1536379062-737-23013-0C766224-0B3D-BC30-B430C77F83D04968__aWxvdmVrZWxseQo_FocalPointCropWzQyMCw5NjAsNDcsNDAsNzUsImpwZyIsNjUsMi41XQ.jpg"
+
+
+# 2
+wget -O /home/kim/Documents/travel-data/images/piazza-san-marco-italy.jpg "https://www.ourescapeclause.com/wp-content/uploads/2021/08/shutterstock_1372373447-scaled.jpg"
+
+
+# 3
+wget -O /home/kim/Documents/travel-data/images/marrakesh-morocco.jpg "https://cdn.thecrazytourist.com/wp-content/uploads/2019/12/ccimage-shutterstock_1390770566.jpg"
+
+
+# 4
+wget -O /home/kim/Documents/travel-data/images/cradle-mountain-australia.jpg "https://www.thewildlifediaries.com/wp-content/uploads/2022/01/Cradle-Mountain.jpg"
+
+
+# 5
+wget -O /home/kim/Documents/travel-data/images/abel-tasman-national-park-new-zealand.jpg "https://www.newzealand.com/assets/Tourism-NZ/Nelson/e4e5325d05/img-1536988589-7965-4682-BA963E64-0277-9F01-148FCD0F9B8DFE49__aWxvdmVrZWxseQo_CropResizeWzUwMCwzNzUsODAsImpwZyJd.jpg"
+
+
+# 6
+wget -O /home/kim/Documents/travel-data/images/old-town-square-czech-republic.jpg "https://www.barcelo.com/guia-turismo/wp-content/uploads/2020/04/plaza-ciudad-vieja-1.jpg"
+
+
+# 7
+wget -O /home/kim/Documents/travel-data/images/bay-of-kotor-montenegro.jpg "https://www.chasingthedonkey.com/wp-content/uploads/2019/01/Port-In-Kotor-Montenegro_shutterstock_1049738972.jpg"
+
+
+# 8
+wget -O /home/kim/Documents/travel-data/images/ngorongoro-conservation-area-tanzania.jpg "https://www.andbeyond.com/wp-content/uploads/sites/5/ngorongoro-crater-floor-teaming-with-game.jpg"
+
+
+# 9
+wget -O /home/kim/Documents/travel-data/images/ha-long-bay-vietnam.jpg "https://media.worldnomads.com/explore/vietnam/halong-bay-vietnam-from-above-gettyimages.jpg"
+
+
+# 10
+wget -O /home/kim/Documents/travel-data/images/ilulissat-greenland.jpg "https://guidetogreenland.com/wp-content/uploads/2020/06/Visit-Greenland-Mads-Pihl-north-ilulissat-accommodation-view-from-hotel-arctic-1024x499.jpg"
+
+
+# 11
+wget -O /home/kim/Documents/travel-data/images/redwood-national-and-state-parks-usa.jpg "https://localadventurer.com/wp-content/uploads/2021/11/what-to-do-at-redwood-national-park.jpg"
+
+
+# 12
+wget -O /home/kim/Documents/travel-data/images/chichen-itza-mexico.jpeg "https://cdn.vallarta-adventures.com/sites/default/files/2021-05/CHichen10.jpeg"
+
+
+# 13
+wget -O /home/kim/Documents/travel-data/images/franz-josef-glacier-new-zealand-new-zealand.jpg "https://www.newzealand.com/assets/Tourism-NZ/West-Coast/6db30df60c/img-1536280982-3035-15788-F384E624-9404-2963-6B0FCA6AEAB2CB13__aWxvdmVrZWxseQo_CropResizeWzUwMCwzNzUsODAsImpwZyJd.jpg"
+
+
+# 14
+wget -O /home/kim/Documents/travel-data/images/niagara-falls-canada-and-usa-canada-and-usa.jpg "https://cdn.getyourguide.com/img/location/60096cdee428e.jpeg/99.jpg"
+
+
+# 15
+wget -O /home/kim/Documents/travel-data/images/great-buddha-of-thailand-thailand-thailand.jpg "https://resources.thomascook.in/images/holidays/sightSeeing/8026141_Great_Buddha_of_Thailand_960.jpg"
+
+
+# 16
+wget -O /home/kim/Documents/travel-data/images/geirangerfjord-norway-norway.jpg "https://cdn.kimkim.com/files/a/content_articles/featured_photos/6d4bab6da3e9de31b4acb091212071de6bd286ff/big-c84d4fc4246e2174873c10f9f8f6f377.jpg"
+
+
+# 17
+wget -O /home/kim/Documents/travel-data/images/kruger-national-park-south-africa-south-africa.jpg "https://cdn.kimkim.com/files/a/content_articles/featured_photos/22d8dd5c4ae3a3626e0cf30b0cc97e48bc14c72d/big-5b902d4507219d542102a964d62a091b.jpg"
+
+
+# 18
+wget -O /home/kim/Documents/travel-data/images/carcassonne-france-france.jpg "https://www.travelrealfrance.com/wp-content/uploads/2021/11/walking-guided-tour-carcassonne-castle-1024x683.jpg"
+
+
+# 19
+wget -O /home/kim/Documents/travel-data/images/vatnajokull-national-park-iceland-iceland.jpg "https://visitvatnajokull.is/wp-content/uploads/2018/03/IMG_8955.jpg"
+
+
+# 20
+wget -O /home/kim/Documents/travel-data/images/terracotta-army-china-china.jpg "https://th-thumbnailer.cdn-si-edu.com/9OMhTzGuUrhksZLoUPi4rUic8qA=/fit-in/1600x0/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/terra-cotta-soldiers-631.jpg"
+
+
+# 21
+wget -O /home/kim/Documents/travel-data/images/phong-nha-ke-bang-national-park-vietnam-vietnam.jpg "https://vietnamdiscovery.com/wp-content/uploads/2019/11/Phong-nha-ke-bang-national-park.jpg"
+
+
+# 22
+wget -O /home/kim/Documents/travel-data/images/accra-ghana-ghana.jpg "https://www.adjaye.com/wp-content/uploads/2020/07/03_Marine-Drive_72.jpg"
+
+
+# 23
+wget -O /home/kim/Documents/travel-data/images/gullfoss-iceland-iceland.jpg "https://adventures.is/media/225849/social-media-gullfoss-waterfall-summer-rainbow.jpg"
+
+
+# 24
+wget -O /home/kim/Documents/travel-data/images/xochimilco-mexico.jpg "https://www.fodors.com/wp-content/uploads/2022/06/Hero-Xolximico-roberto-carlos-roman-don-_N6BAsuUqBc-unsplash.jpg"
+
+
+# 25
+wget -O /home/kim/Documents/travel-data/images/glencoe-highland-scotland.jpg "https://kittiaroundtheworld.com/wp-content/uploads/2021/09/Glencoe.jpg"
+
+
+# 26
+wget -O /home/kim/Documents/travel-data/images/isimangaliso-wetland-park-south-africa.jpg "https://www.isibindi.co.za/wp-content/uploads/sites/9/2018/01/Kosi-Forest-Mouth-Resized.jpg"
+
+
+# 27
+wget -O /home/kim/Documents/travel-data/images/old-tbilisi-georgia.jpg "https://www.enterprise.ge/uploads/2020/01/tbilisi-large.jpg"
+
+
+# 28
+wget -O /home/kim/Documents/travel-data/images/blue-mountains-new-south-wales-australia.jpg "https://static1.evcdn.net/images/reduction/1254050_w-850_h-400_q-100_m-crop.jpg"
+
+
+# 29
+wget -O /home/kim/Documents/travel-data/images/lake-atitlan-guatemala.jpg "https://www.atastefortravel.ca/wp-content/uploads/2020/09/Lake-Atitlan-DP-1200x618.jpg"
+
+
+# 30
+wget -O /home/kim/Documents/travel-data/images/gunung-leuser-national-park-indonesia.jpg "https://indonesiaexpat.biz/wp-content/uploads/2019/07/DSC_9699-e1563261669594.jpg"
+
+
+# 31
+wget -O /home/kim/Documents/travel-data/images/rijksmuseum-netherlands.jpg "https://cdn.getyourguide.com/img/tour/5db0759f4ad16.jpeg/99.jpg"
+
+
+# 32
+wget -O /home/kim/Documents/travel-data/images/pamir-mountains-tajikistan.jpg "https://www.naturalworldheritagesites.org/wp-content/uploads/2021/04/Tajik-National-Park-Mountains-of-the-Pamirs.jpg"
--- wiki.py
+++ wiki.py
@@ -1,14 +1,4 @@
-
-import json
-from decimal import Decimal
-
-#Does quasi the same things as json.loads from here: https://pypi.org/project/dynamodb-json/
-class JSONEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, Decimal):
-            return float(obj)
-        return json.JSONEncoder.default(self, obj)
-
+ 
 
 import wikipedia
 #print(wikipedia.summary("Albert Einstein", sentences=2))
